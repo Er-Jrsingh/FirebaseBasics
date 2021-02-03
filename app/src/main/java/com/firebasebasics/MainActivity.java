63ac2fee8f2d745d1ececfbb63f47fef57ac5426
Jitesh Singh <jitust98@gmail.com>
2021-02-03 21:44:15 +0530

    Insert a message In the Firebase database Root Node

--- app/src/main/java/com/firebasebasics/MainActivity.java
+++ app/src/main/java/com/firebasebasics/MainActivity.java
@@ -4,12 +4,21 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Write a message to the Firebase database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("");
+
+        myRef.setValue("Hola !!  FireB !!");
 
     }
 }
