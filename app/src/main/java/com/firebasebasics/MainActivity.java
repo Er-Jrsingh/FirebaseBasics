95c097e5b0a46d6abf50e81014fcb0483395ef75
Jitesh Singh <jitust98@gmail.com>
2021-02-03 22:07:57 +0530

    Fetch Stored message from the Firebase database and Display on Toast

--- app/src/main/java/com/firebasebasics/MainActivity.java
+++ app/src/main/java/com/firebasebasics/MainActivity.java
@@ -1,13 +1,21 @@
 package com.firebasebasics;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG ="err" ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,25 @@ public class MainActivity extends AppCompatActivity {
         DatabaseReference myRef = database.getReference("FirstyNode");
 
         myRef.setValue("Hola !!  FireB !!");
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+
+                String value = dataSnapshot.getValue(String.class);
+                Toast.makeText(getApplicationContext(),value,Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+                Toast.makeText(getApplicationContext(),"Failed to read value",Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 }
